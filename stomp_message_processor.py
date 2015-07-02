4d98427db142c81cfcc29159a2c0ff115dd99eb3
Mark Aufdencamp <Mark@Aufdencamp.com>
2015-07-02 15:07:38 -0400

    Added stomp_message_router instance attribute to stomp_message_processor.  Added build_route_table and print_route_table methods.  Factoried stomp_message_router instance and invoked build_route_table in constructor.

--- stomp_message_processor.py
+++ stomp_message_processor.py
@@ -3,16 +3,24 @@
 #
 # Controllers should be subclass of StompMessageController
 # StompMessageProcessor loads classes from disk and instantiates an instance
-# StompMessageController registers controller Tasks with StompMessageProcessor 
+# StompMessageController constructor registers controller Tasks with StompMessageProcessor 
 
 import pkgutil
 import sys
 import os
 
 from stomp_message_controller import StompMessageController
-from stomp_message_controller import StompMessage
+from stomp_message import StompMessage
 
 class StompMessageProcessor:
+
+	def __init__(self, controller_dir, stomp_message_router):
+		# Load Controllers
+		self.controller_dir = controller_dir
+		self.stomp_message_router = stomp_message_router
+		self.processor_tasks = dict()
+		self.load_stomp_controllers(controller_dir)
+		self.build_route_table()
 
 	# Build class name from module file name
 	def get_class_name(self, mod_name):
@@ -37,8 +45,6 @@ class StompMessageProcessor:
 	def load_stomp_controllers(self, controller_dir):
 		print("Loading controllers - " + controller_dir)
 		#TODO: Validate controller_dir existence
-		#path = os.path.join(os.path.dirname(__file__), controller_dir)
-		#path = os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), controller_dir)
 		path = controller_dir
 		modules = pkgutil.iter_modules(path=[path])
 		for loader, mod_name, ispkg in modules:
@@ -48,17 +54,16 @@ class StompMessageProcessor:
 			#TODO: Wrap class instantiation with exception handler
 			instance = self.load_stomp_controller_class(loaded_mod, class_name)
 
-			instance.run()
+			#instance.run()
 			instance.register_stomp_tasks(self)
 			#instance.print_stomp_tasks()
 
 	def reload_stomp_controller():
 		print("Reload Stomp controller not yet implemented")
 
-	def print_routes(self):
-		for task in self.processor_tasks:
-			print(task, self.processor_tasks[task])
-
+	def tasks(self):
+		return self.processor_tasks
+				
 	def route_add(self, task, controller):
 		#TODO: Validate Task has not been registered by another controller
 		self.processor_tasks[task] = controller
@@ -66,11 +71,13 @@ class StompMessageProcessor:
 	def route_delete(self, task):
 		del self.processor_tasks[task]
 
-	def __init__(self, controller_dir):
-		# Load Controllers
-		self.controller_dir = controller_dir
-		self.processor_tasks = dict()
-		self.load_stomp_controllers(controller_dir)
+	def build_route_table(self):
+		for task in self.processor_tasks:
+			self.stomp_message_router.add_routes(self, self.processor_tasks)
+
+	def print_routes(self):
+		for task in self.processor_tasks:
+			print(task, self.processor_tasks[task])
 
 
 #TODO: Read and parse CLI arguments
