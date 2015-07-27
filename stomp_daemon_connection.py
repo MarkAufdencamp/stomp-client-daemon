b00bbd50a92d223ad38e813886d7b8292e65c2e7
Mark Aufdencamp <Mark@Aufdencamp.com>
2015-07-27 01:38:06 -0400

    Refactored StompDaemonConnection to be utilized by StompDaemon.  Initial class was composed of methods extracted from StompDaemon.

--- stomp_daemon_connection.py
+++ stomp_daemon_connection.py
@@ -1,51 +1,73 @@
-class StompDaemonConnection():
-	def __init__(self):
-		print("StompDaemonConnection constructor")
+import sys
+import time
+import stomp
+from stomp_daemon_listener import StompDaemonListener
 
-	def stompConnect(self, msgSrvr, msgSrvrPort, msgSrvrClientId, stompMessageRouter, threadPool):
+class StompDaemonConnection():
+
+# self.msgSrvr
+# self.msgSrvrPort
+# self.msgSrvrClientId
+# self.msgSrvrQueues
+# self.stompConn
+# self.stompDaemonListener
+
+	def __init__(self, msgSrvr, msgSrvrPort, msgSrvrClientId):
+		print("StompDaemonConnection constructor")
+		self.msgSrvr = msgSrvr
+		self.msgSrvrPort = msgSrvrPort
+		self.msgSrvrClientId = msgSrvrClientId
+		self.msgSrvrQueues = []
+
+	def stompConnect(self, stompDaemonListener):
 		# Connect via Stomp to the Message Server
 		# Factory Stomp connection
-		sys.stdout.write('Connecting to message server - {0}:{1} - {2}\n'.format(msgSrvr, msgSrvrPort, time.ctime()))
-		global stompConn, stompDaemonListener
+		sys.stdout.write('Connecting to message server - {0}:{1} {2} - {3}\n'.format(self.msgSrvr, self.msgSrvrPort, self.msgSrvrClientId, time.ctime()))
 
 		try:
-			stompConn = stomp.Connection( host_and_ports=[(msgSrvr, msgSrvrPort)], heartbeats=(60000, 20000) )
-			stompDaemonListener = StompDaemonListener(stompMessageRouter, threadPool)
-			stompConn.set_listener('StompDaemonListener', stompDaemonListener)
+			self.stompConn = stomp.Connection( host_and_ports=[(self.msgSrvr, self.msgSrvrPort)], heartbeats=(60000, 20000) )
+			self.stompDaemonListener = stompDaemonListener
+			self.stompConn.set_listener('StompDaemonListener', stompDaemonListener)
 		except Exception as err:
 			print('Message Server Connection Error')
 
 		try:
-			stompConn.start()
+			self.stompConn.start()
 		except Exception:
 			print('Message Server Start Error')
 
 		try:
-			stompConn.connect(headers={'client-id': msgSrvrClientId})
+			self.stompConn.connect(headers={'client-id': self.msgSrvrClientId})
 		except Exception:
 			print('Message Server Connect Error')
 
-	def stompDisconnect(self, stompConn):
+	def stompDisconnect(self):
 		sys.stdout.write('Disconnecting from message server {}\n'.format(time.ctime()))
+		#TODO: Remove Listener
 		try:
-			stompConn.disconnect()
+			self.stompConn.disconnect()
 		except Exception:
 			print('Message Server Disconnect Error')
 
-	def stompSubscribe(self, stompConn, msgSrvrQueue):
+	def stompSubscribe(self, msgSrvrQueue):
 		# Subscribe to the configured message queue
 		sys.stdout.write('Subscribing to message queue - {0} - {1}\n'.format(msgSrvrQueue, time.ctime()))
 		try:
-			stompConn.subscribe(destination=msgSrvrQueue, id=1, ack='auto')
+			self.stompConn.subscribe(destination=msgSrvrQueue, id=1, ack='auto')
+			if (msgSrvrQueue not in self.msgSrvrQueues):
+				self.msgSrvrQueues.append(msgSrvrQueue)
 		except Exception:
 			print("Message Queue Subscribe Error")
 
-	def stompUnsubscribe(self, stompConn, msgSrvrQueue):
+	def stompUnsubscribe(self, msgSrvrQueue):
 		# Unsubscribe to the configured message queue
 		sys.stdout.write('Unsubscribing from message queue - {0} - {1}\n'.format(msgSrvrQueue, time.ctime()))
 		try:
-			stompConn.unsubscribe(id=msgSrvrQueue)
+			self.stompConn.unsubscribe(id=msgSrvrQueue)
+			if (msgSrvrQueue in self.msgSrvrQueues):
+				self.msgSrvrQueues.remove(msgSrvrQueue)
 		except Exception:
 			print("Message Queue Unsubscribe Error")
 
-
+	def stompSendMessage(self, msgSrvrQueue, stompMessage):
+		print("StompDaemonConnection stompSendMessage")	
