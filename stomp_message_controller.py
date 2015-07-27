4539978ee500dfef33354b9adbf7512bcfab143d
Mark Aufdencamp <Mark@Aufdencamp.com>
2015-07-27 01:43:02 -0400

    Added set_stomp_message_broker method to StompMessageController.  Utilized for injection of StompMessageBroker after instantiation.  Added __getattr__ method to StompMessageController inorder to catch the missing method if a route exists in a controller, but the method does not.

--- stomp_message_controller.py
+++ stomp_message_controller.py
@@ -1,4 +1,7 @@
 class StompMessageController:
+	def set_stomp_message_broker(self, stompMessageBroker):
+		self.stompMessageBroker = stompMessageBroker
+
 	def register_stomp_tasks(self, stomp_processor):
 		for task in self.stomp_tasks:
 			stomp_processor.route_add(task, self)
@@ -15,3 +18,10 @@ class StompMessageController:
 		print()
 		print(stomp_message)
 		print()
+
+	def __getattr__(self, name):
+		def _missing(*args, **kwargs):
+			print("A missing method was called.")
+			print("The Object was {0} and the method was {1}".format(self, name))
+			print("It was called with {0} and {1}".format(args, kwargs))
+		return _missing
