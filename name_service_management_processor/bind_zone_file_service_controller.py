21e4c898ebf0e708ef18e9a4066a27958b6eb40d
Mark Aufdencamp <Mark@Aufdencamp.com>
2017-03-31 17:14:00 -0400

    Added acknowledge_success/acknowledge_failure methods to BindZoneFileServiceController.  Mocked successfulProvisioning for create/remove/enable/disable bind_zone_file methods.

--- name_service_management_processor/bind_zone_file_service_controller.py
+++ name_service_management_processor/bind_zone_file_service_controller.py
@@ -11,15 +11,63 @@ class BindZoneFileServiceController(StompMessageController):
 	def create_bind_zone_file(self, stomp_message):
 		print("CreateBindZoneFile - bind_zone_file_service_controller.create_bind_zone_file()")
 		stomp_message.print_message()
+		
+		successfulProvisioning = True
+		
+		acknowledgementMessageDict = stomp_message.parsed_body
+		acknowledgementQueue = acknowledgementMessageDict["service"]
+		
+		if successfulProvisioning:
+			stomp_message.acknowledge_success(acknowledgementQueue, acknowledgementMessageDict)
+		else:
+			stomp_message.acknowledge_failure(acknowledgementQueue, acknowledgementMessageDict)
 
 	def remove_bind_zone_file(self, stomp_message):
 		print("RemoveBindZone - bind_zone_file_service_controller.remove_bind_zone_file()")
 		stomp_message.print_message()
 
+		successfulProvisioning = True
+		
+		acknowledgementMessage = stomp_message.parsed_body
+		acknowledgementQueue = acknowledgementMessage["service"]
+		
+		if successfulProvisioning:
+			stomp_message.acknowledge_success(acknowledgementQueue, acknowledgementMessage)
+		else:
+			stomp_message.acknowledge_failure(acknowledgementQueue, acknowledgementMessage)
+
 	def enable_bind_zone_file(self, stomp_message):
 		print("EnableBindZone - bind_zone_file_service_controller.enable_bind_zone_file()")
 		stomp_message.print_message()
 
+		successfulProvisioning = True
+		
+		acknowledgementMessage = stomp_message.parsed_body
+		acknowledgementQueue = acknowledgementMessage["service"]
+		
+		if successfulProvisioning:
+			stomp_message.acknowledge_success(acknowledgementQueue, acknowledgementMessage)
+		else:
+			stomp_message.acknowledge_failure(acknowledgementQueue, acknowledgementMessage)
+
 	def disable_bind_zone_file(self, stomp_message):
 		print("DisableBindZone - bind_zone_file_service_controller.disable_bind_zone_file()")
 		stomp_message.print_message()
+
+		successfulProvisioning = True
+		
+		acknowledgementMessage = stomp_message.parsed_body
+		acknowledgementQueue = acknowledgementMessage["service"]
+		
+		if successfulProvisioning:
+			stomp_message.acknowledge_success(acknowledgementQueue, acknowledgementMessage)
+		else:
+			stomp_message.acknowledge_failure(acknowledgementQueue, acknowledgementMessage)
+
+	def acknowledge_success(self, queue, message):
+		print("== Acknowledge Message Success ==")
+		self.send_message(queue, message)
+	
+	def acknowledge_failure(self, queue, message):
+		print("== Acknowledge Message Failure ==")
+		self.send_message(queue, message)
