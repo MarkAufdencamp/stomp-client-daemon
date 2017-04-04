6054c06cc59499ea0125cb573e1c1660db84efbd
MarkAufdencamp <Mark@Aufdencamp.com>
2017-04-04 21:59:40 +0000

    Fixed CamelCase to SnakeCase method names in MySQLDatabaseServiceController.  May be portability issue to keep an eye on!

--- database_management_processor/mysql_database_service_controller.py
+++ database_management_processor/mysql_database_service_controller.py
@@ -8,30 +8,30 @@ class MysqlDatabaseServiceController(StompMessageController):
 	def run(self):
 		print("MysqlDatabaseServiceController")
 
-	def create_my_s_q_l_database(self, stomp_message):
-		print("CreateMySQLDatabase - mysql_database_service_controller.create_my_s_q_l_database()")
+	def create_my_sql_database(self, stomp_message):
+		print("CreateMySQLDatabase - mysql_database_service_controller.create_my_sql_database()")
 		stomp_message.print_message()
 
-	def remove_my_s_q_l_database(self, stomp_message):
-		print("RemoveMySQLDatabase - mysql_database_service_controller.remove_my_s_q_l_database()")
+	def remove_my_sql_database(self, stomp_message):
+		print("RemoveMySQLDatabase - mysql_database_service_controller.remove_my_sql_database()")
 		stomp_message.print_message()
 
-	def enable_my_s_q_l_database(self, stomp_message):
-		print("EnableMySQLDatabase - mysql_database_service_controller.enable_my_s_q_l_database()")
+	def enable_my_sql_database(self, stomp_message):
+		print("EnableMySQLDatabase - mysql_database_service_controller.enable_my_sql_database()")
 		stomp_message.print_message()
 
-	def disable_my_s_q_l_database(self, stomp_message):
-		print("DisableMySQLDatabase - mysql_database_service_controller.disable_my_s_q_l_database()")
+	def disable_my_sql_database(self, stomp_message):
+		print("DisableMySQLDatabase - mysql_database_service_controller.disable_my_sql_database()")
 		stomp_message.print_message()
 
-	def set_my_s_q_l_database_passwd(self, stomp_message):
-		print("SetMySQLDatabasePasswd - mysql_database_service_controller.set_my_s_q_l_database_passwd()")
+	def set_my_sql_database_passwd(self, stomp_message):
+		print("SetMySQLDatabasePasswd - mysql_database_service_controller.set_my_sql_database_passwd()")
 		stomp_message.print_message()
 
-	def backup_my_s_q_l_database(self, stomp_message):
-		print("BackupMySQLDatabase - mysql_database_service_controller.backup_my_s_q_l_database()")
+	def backup_my_sql_database(self, stomp_message):
+		print("BackupMySQLDatabase - mysql_database_service_controller.backup_my_sql_database()")
 		stomp_message.print_message()
 
-	def restore_my_s_q_l_database(self, stomp_message):
-		print("RestoreMySQLDatabase - mysql_database_service_controller.restore_my_s_q_l_database()")
+	def restore_my_sql_database(self, stomp_message):
+		print("RestoreMySQLDatabase - mysql_database_service_controller.restore_my_sql_database()")
 		stomp_message.print_message()
